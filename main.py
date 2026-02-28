d291f5d73713b3e0363d1bf03c0ed4df9b867ecd
Prasad Desai <desaiprasad19@gmail.com>
2026-02-28 17:15:27 +0530

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from wdmproject import logger
+from wdmproject.pipeline.stage_01_data_ingestion_pipeline import DataIngestionPipeline
 
-logger.info("This is a Custom log message")
+
+STAGE_NAME   = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion_pipeline = DataIngestionPipeline()
+    data_ingestion_pipeline.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
